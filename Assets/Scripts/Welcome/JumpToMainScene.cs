2cebc7d0ff33bfc67c63a1ba9f978112d727258e
NachtgeistW <nachtgeistw@protonmail.com>
2020-07-30 23:21:27 +0800

    2020.7.30 init git

---
+++ Assets/Scripts/Welcome/JumpToMainScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpToMainScene : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("SongSelectScene");
+    }
+}
