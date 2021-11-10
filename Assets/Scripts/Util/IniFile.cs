6d129ad78e14186a939d5ea18c60c98e39948bb5
Blurring Shadow <976999484@qq.com>
2021-11-10 22:38:51 +0800

    feat(util): add ini file load extension

---
+++ Assets/Scripts/Util/IniFile.cs
@@ -0,0 +1,10 @@
+namespace Util
+{
+    using Microsoft.Extensions.Configuration;
+
+    public static class IniFile
+    {
+        public static IConfigurationRoot FromPath(string path) =>
+            new ConfigurationBuilder().AddIniFile(path).Build();
+    }
+}
