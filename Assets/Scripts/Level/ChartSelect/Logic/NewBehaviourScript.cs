c8065d00c6618eb58da065efabd6a2b1655dc301
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2023-06-26 11:22:10 +0800

    fix: fix a bug where translucent image doesn't display

--- Assets/Scripts/Level/ChartSelect/Logic/NewBehaviourScript.cs
+++ Assets/Scripts/Level/ChartSelect/Logic/NewBehaviourScript.cs
@@ -1,42 +1,42 @@
-using Plutono.IO;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using AwesomeCharts;
+//using Plutono.IO;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using AwesomeCharts;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    MusicBarCaculator MusicBarCaculator = new MusicBarCaculator();
-    public BarChart barChart;
-    // Start is called before the first frame update
-    void Start()
-    {
-        var noteDetail = FileManager.Instance.songSourceList[0].ChartDetails[0].noteDetails;
-        var musicSource = AudioClipFileManager.Read(FileManager.Instance.songSourceList[0].MusicPath);
+//public class NewBehaviourScript : MonoBehaviour
+//{
+//    MusicBarCaculator MusicBarCaculator = new MusicBarCaculator();
+//    public BarChart barChart;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        var noteDetail = FileManager.Instance.songSourceList[0].ChartDetails[0].noteDetails;
+//        var musicSource = AudioClipFileManager.Read(FileManager.Instance.songSourceList[0].MusicPath);
 
-        MusicBarCaculator.calculate(musicSource.length, noteDetail);
-        BarDataSet set = new();
-        for (int i = 0; i < MusicBarCaculator.maxBar - 1; i++)
-        {
-            set.AddEntry(new BarEntry(i, MusicBarCaculator.GetBar(i)));
-        }
-        barChart.GetChartData().DataSets.Add(set);
+//        MusicBarCaculator.calculate(musicSource.length, noteDetail);
+//        BarDataSet set = new();
+//        for (int i = 0; i < MusicBarCaculator.maxBar - 1; i++)
+//        {
+//            set.AddEntry(new BarEntry(i, MusicBarCaculator.GetBar(i)));
+//        }
+//        barChart.GetChartData().DataSets.Add(set);
         
-        BarChartConfig config = new BarChartConfig
-        {
-            BarWidth = 40,
-            BarSpacing = 20,
-            InnerBarSpacing = 10,
-            SizingMethod = BarSizingMethod.SIZE_TO_FIT
-        };
-        barChart.Config = config;
+//        BarChartConfig config = new BarChartConfig
+//        {
+//            BarWidth = 40,
+//            BarSpacing = 20,
+//            InnerBarSpacing = 10,
+//            SizingMethod = BarSizingMethod.SIZE_TO_FIT
+//        };
+//        barChart.Config = config;
         
-        barChart.SetDirty();
-    }
+//        barChart.SetDirty();
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
-}
+//    }
+//}
